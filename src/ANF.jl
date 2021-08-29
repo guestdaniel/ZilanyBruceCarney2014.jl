2877e5a098bf1dc2b77033b523728d8e4862335b
Daniel Guest <guestusut@gmail.com>
2021-08-29 16:09:05 -0500

    Added nicer wrapper to SingleAN! and updated tests
    Added several new tests to check basic responses of auditory nerve simulations
    and added a wrapper to SingleAN! so that the user can generate analytic firing
    rate simulations.

--- src/ANF.jl
+++ src/ANF.jl
@@ -26,6 +26,9 @@ Simulates inner hair cell potential for given acoustic input.
 - `cohc::Float64`: outer hair cell survival (from 0 to 1)
 - `cihc::Float64`: inner hair cell survival (from 0 to 1)
 - `species::Int32`: species, either (1 = cat, 2 = humans with Shera tuning, 3 = humans with Glasberg tuning)
+
+# Returns
+- `output::Array{Float64, 1}`: inner hair cell potential output
 """
 function sim_ihc_zbc2014(input::Array{Float64, 1}, cf::Float64; fs::Float64=10e4,
                          cohc::Float64=1.0, cihc::Float64=1.0, species::String="cat")
@@ -41,14 +44,20 @@ end
 
 
 """
-    sim_an_zbc2014(input, cf; fs=10e4, cohc=1.0, cihc=1.0, species)
+    sim_synapse_zbc2014(input, cf; fs=10e4, cohc=1.0, cihc=1.0)
+
+Simulates synapse output for a given inner hair cell input
+
 # Arguments
 - `input::Array{Float64, 1}`: input hair cell potential (from sim_ihc_zbc2014)
 - `cf::Float64`: characteristic frequency of the fiber in Hz
 - `fiber_type::String`: fiber type, one of ("low", "medium", "high") spontaneous rate
 - `frac_noise::String`: controls whether we use true or approximate fractional Gaussian noise implementation, one of ("actual", "approximate")
+
+# Returns
+- `output::Array{Float64, 1}`: synapse output (unknown units?)
 """
-function sim_an_zbc2014(input::Array{Float64, 1}, cf::Float64; fs::Float64=10e4,
+function sim_synapse_zbc2014(input::Array{Float64, 1}, cf::Float64; fs::Float64=10e4,
                         fiber_type::String="high", frac_noise::String="approximate")
     # Map fiber type string to float code expected by Synapse!
     spont = Dict([("low", 0.1), ("medium", 4.0), ("high", 100.0)])[fiber_type]
@@ -61,6 +70,40 @@ function sim_an_zbc2014(input::Array{Float64, 1}, cf::Float64; fs::Float64=10e4,
     # Return
     return output
 end
+
+
+"""
+    sim_an_zbc2014(input, cf; fs=10e4, cohc=1.0, cihc=1.0)
+
+Simulates auditory nerve output (spikes or firing rate) for a given inner hair cell input
+
+# Arguments
+- `input::Array{Float64, 1}`: input hair cell potential (from sim_ihc_zbc2014)
+- `cf::Float64`: characteristic frequency of the fiber in Hz
+- `fiber_type::String`: fiber type, one of ("low", "medium", "high") spontaneous rate
+- `frac_noise::String`: controls whether we use true or approximate fractional Gaussian noise implementation, one of ("actual", "approximate")
+
+# Returns
+- `meanrate::Array{Float64, 1}`: analytical estimate of instantaneous firing rate
+- `varrrate::Array{Float64, 1}`: analytical estimate of instantaneous firing rate variance
+- `psth::Array{Float64, 1}`: peri-stimulus time histogram (NOT IMPLEMENTED, SHOULD BE EMPTY)
+"""
+function sim_an_zbc2014(input::Array{Float64, 1}, cf::Float64; fs::Float64=10e4,
+                        fiber_type::String="high", frac_noise::String="approximate")
+    # Map fiber type string to float code expected by Synapse!
+    fibertype = Dict([("low", 1.0), ("medium", 2.0), ("high", 3.0)])[fiber_type]
+    # Map fractional noise implementation type to float code expected by Syanpse!
+    implnt = Dict([("actual", 1.0), ("approximate", 0.0)])[frac_noise]
+    # Create empty array for output
+    meanrate = zeros((length(input), ))
+    varrate = zeros((length(input), ))
+    psth = zeros((length(input), ))
+    # Make call
+    SingleAN!(input, cf, Int32(1), 1.0/fs, Int32(length(input)), fibertype, 1.0, implnt, meanrate, varrate, psth)
+    # Return
+    return (meanrate, varrate, psth)
+end
+
 
 
 """
@@ -150,11 +193,11 @@ automatically by Julia, there are no sanity checks on any arguments.
 - `varrate::Array{Float64, 1}`: array of same size as `ihcout`, used to store analytical firing rate variance output
 - `psth::Array{Float64, 1}`: array of same size as `ihcout`, used to store empirical PSTH output
 """
-function SingleAN(ihcout::Array{Float64, 1}, cf::Float64, nrep::Int32,
-                  tdres::Float64, totalstim::Int32, fibertype::Float64,
-                  noiseType::Float64, implnt::Float64,
-                  meanrate::Array{Float64, 1}, varrate::Array{Float64, 1},
-                  psth::Array{Float64, 1})
+function SingleAN!(ihcout::Array{Float64, 1}, cf::Float64, nrep::Int32,
+                   tdres::Float64, totalstim::Int32, fibertype::Float64,
+                   noiseType::Float64, implnt::Float64,
+                   meanrate::Array{Float64, 1}, varrate::Array{Float64, 1},
+                   psth::Array{Float64, 1})
     ccall((:SingleAN, libihc), Cvoid, (Ptr{Cdouble}, Cdouble, Cint, Cdouble,
                                        Cint, Cdouble, Cdouble, Cdouble,
                                        Ptr{Cdouble}, Ptr{Cdouble},
---
+++ test/runtests.jl
@@ -0,0 +1,127 @@
+"""
+This testing suite handles very basic tests of the implemented auditory nerve models... Namely, it makes sure that they can be run without error using
+default arguments and empty/standard inputs and it makes sure that basic response properties (e.g., spontaneous rate and rate-level function) are
+approximately correct (i.e., exact spontaneous rates are not checked, but relative spontaneous rates like high > low are checked).
+"""
+
+using Test
+using ANF
+using AuditorySignalUtils
+const ASU = AuditorySignalUtils
+using Statistics
+
+# Declare various constants that hold across all tests in this file
+fs = 100000.0
+dur = 0.1
+freq = 1000.0
+pt = ASU.scale_dbspl(ASU.pure_tone(freq, 0.0, dur, fs), 50.0)
+
+# Start by testing the direct bindings and just make sure that they run!
+@testset "C bindings: check callable" begin
+  # First, we try testing the direct C binding to the IHC code (IHCAN!)
+  @test begin
+      px = pt
+      cf = freq
+      nrep = Int32(1)
+      tdres = 1.0/fs
+      totalstim = Int32(dur*fs)
+      cohc = 1.0
+      cihc = 1.0
+      species = Int32(1)
+      ihcout = Vector{Cdouble}(zeros((Int64(dur*fs), )))
+      ANF.IHCAN!(px, cf, nrep, tdres, totalstim, cohc, cihc, species, ihcout)
+      true
+  end
+
+  # Next, we try testing the direct C binding to the Synapse code (Synapse!)
+  @test begin
+      px = pt
+      tdres = 1.0/fs
+      cf = freq
+      totalstim = Int32(dur*fs)
+      nrep = Int32(1)
+      spont = 100.0
+      noiseType = 1.0
+      implnt = 0.0
+      sampFreq = 50000.0
+      cohc = 1.0
+      cihc = 1.0
+      species = Int32(1)
+      ihcout = Vector{Cdouble}(zeros((Int64(dur*fs), )))
+      synouttmp = Vector{Cdouble}(zeros((Int64(dur*fs), )))
+
+      ANF.IHCAN!(px, cf, nrep, tdres, totalstim, cohc, cihc, species, ihcout)
+      ANF.Synapse!(ihcout, tdres, cf, totalstim, nrep, spont, noiseType, implnt,
+                       sampFreq, synouttmp)
+      true
+  end
+
+  # Finally, we try testing the direct C binding to the SingleAN!() function
+  @test begin
+      px = ASU.scale_dbspl(ASU.pure_tone(freq, 0.0, dur, fs), 50.0)
+      tdres = 1.0/fs
+      cf = freq
+      totalstim = Int32(dur*fs)
+      nrep = Int32(1)
+      spont = 100.0
+      fibertype = 3.0
+      noiseType = 1.0
+      implnt = 0.0
+      sampFreq = 50000.0
+      cohc = 1.0
+      cihc = 1.0
+      species = Int32(1)
+      ihcout = Vector{Cdouble}(zeros((Int64(dur*fs), )))
+      synouttmp = Vector{Cdouble}(zeros((Int64(dur*fs), )))
+      meanrate = Vector{Cdouble}(zeros((Int64(dur*fs), )))
+      varrate = Vector{Cdouble}(zeros((Int64(dur*fs), )))
+      psth = Vector{Cdouble}(zeros((Int64(dur*fs), )))
+
+      ANF.IHCAN!(px, cf, nrep, tdres, totalstim, cohc, cihc, species, ihcout)
+      ANF.SingleAN!(ihcout, cf, nrep, tdres, totalstim, fibertype, noiseType,
+                       implnt, meanrate, varrate, psth)
+      true
+  end
+end
+
+# Next we test the "wrappers" that provide a nicer interface to the bindings, beginning with simple checks that they are callable
+@testset "Wrappers: check callable" begin
+    # Check if the wrapper can be evaluated
+    @test begin
+        ANF.sim_ihc_zbc2014(zeros((Int(dur*fs), )), freq)
+        true
+    end
+    @test begin
+        ANF.sim_synapse_zbc2014(zeros((Int(dur*fs), )), freq)
+        true
+    end
+    @test begin
+        ANF.sim_an_zbc2014(zeros((Int(dur*fs), )), freq)
+        true
+    end
+end
+# Next we check that the outputs are sensible
+@testset "Wrappers: outputs" begin
+    # Check that inner hair cell response shows expected hallmarks (response to sinusoid at CF, partial rectification)
+    @test begin
+        output = ANF.sim_ihc_zbc2014(pt, freq)
+        # Check that
+        #1 at least one element is non-zero,
+        #2 minima is smaller abs val than maxima (consistent with partial rectification in IHC)
+        any(output .!= 0) && (abs(minimum(output)) < abs(maximum(output)))
+    end
+    # Check that auditory nerve response shows expected pattern of results with changing spontaneous rate
+    @test begin
+        ihcout = ANF.sim_ihc_zbc2014(pt, freq)
+        output = map(Statistics.mean, [ANF.sim_an_zbc2014(ihcout, freq, fiber_type=_type)[1] for _type in ["low", "medium", "high"]])
+        output[3] > output[2] > output[1]
+    end
+    # Check that auditory nerve shows reasonable rate-level function (e.g., range from 10 to 30 dB is constantly increasing)
+    @test begin
+        inputs = [ASU.scale_dbspl(pt, level) for level in [10.0, 15.0, 20.0, 25.0, 30.0]]
+        ihcouts = [ANF.sim_ihc_zbc2014(input, freq) for input in inputs]
+        outputs = [ANF.sim_an_zbc2014(ihcout, freq)[1] for ihcout in ihcouts]
+        means = map(Statistics.mean, outputs)
+        means[5] > means[4] > means[3] > means[2] > means[1]
+    end
+end
