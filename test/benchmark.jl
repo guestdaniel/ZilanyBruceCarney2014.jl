a394092a2be28dfae665af9e511886429a4b530c
Daniel Guest <guestusut@gmail.com>
2021-09-01 11:14:06 -0500

    Removed profiling code from benchmarking script
    Removed profiling code from benchmarking script but left the @benchmark
    part in. This script will be used to collect tests of running speed for
    my own use and to report in the README.

--- test/benchmark.jl
+++ test/benchmark.jl
@@ -1,25 +1,12 @@
 using BenchmarkTools
-using Profile
-using ProfileView
-include("../src/ZBC2014.jl")
-include("../src/Hearing.jl")
+using ANF
+using AuditorySignalUtils
+const ASU = AuditorySignalUtils
 
 # Declare various constants that hold across all tests in this file
 fs = 100000.0
 dur = 1.0
-pt = Hearing.scale_dbspl(Hearing.pure_tone(1000.0, 0.0, dur, fs), 50.0)
+pt = ASU.scale_dbspl(ASU.pure_tone(1000.0, 0.0, dur, fs), 50.0)
 
 # Benchmark running a sinusoid several times
-@benchmark ZBC2014.sim_ihc_zbc2014(zeros((Int(dur*fs), )), 1000.0)
-
-@time ZBC2014.sim_ihc_zbc2014(pt, repeat([1000.0], 100));
-@profile ZBC2014.sim_ihc_zbc2014(pt, repeat([1000.0], 100));
-
-input = pt
-species_flag = ZBC2014.translate_species_label("cat")
-cohc = 1.0
-cihc = 1.0
-output = zeros((length(input), ))
-cf = 1000.0
-@time ZBC2014.IHCAN!(input, cf, Int32(1), 1/fs, Int32(length(input)), cohc, cihc, Int32(species_flag), output)
-
+@benchmark ANF.sim_ihc_zbc2014(zeros((Int(dur*fs), )), 1000.0)
