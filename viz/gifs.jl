e7ce5af4c2b04d55fa2c7b9ca125b3050db1c49d
Daniel Guest <guestusut@gmail.com>
2021-09-09 16:25:27 -0500

    Playing with some gifs

--- viz/gifs.jl
+++ viz/gifs.jl
@@ -5,6 +5,9 @@ using AuditoryNerveFiber
 const ANF = AuditoryNerveFiber
 using Plots
 
+# ===== A few handy functions ========================
+LogRange(a, b, n) = 10 .^ LinRange(log10(a), log10(b), n)
+
 # ===== Plot #1 ======================================
 # Plot ANF response as function of level for pure tone
 # ====================================================
@@ -25,3 +28,28 @@ anim = @animate for level in levels
     plot_an(level)
 end
 gif(anim, fps=10)
+
+# ===== Plot #2 ======================================
+# Plot ANF response as function of CF for pure tone
+# ====================================================
+# Define range of levels
+CFs = LinRange(200.0, 2000.0, 30)
+
+# Define function that plots what we want
+function plot_an(cf)
+    x = ASU.scale_dbspl(ASU.pure_tone(1000.0, 0.0, 0.1, 100_000.0), 30.0)
+    x = [zeros(500); x; zeros(500)]
+    y = ANF.sim_an_zbc2014(x, cf)[1]
+    plot(y[250:2000], ylims=(0, 1500))
+    annotate!((250, 1250, string(round(cf; sigdigits=2))))
+end
+
+# Create gif
+anim = @animate for cf in CFs
+    plot_an(cf)
+end
+gif(anim, fps=10)
+
+# excitation pattern
+x = ASU.scale_dbspl(ASU.pure_tone(1000.0, 0.0, 0.1, 100_000.0), 30.0)
+plot(map(cf -> mean(ANF.sim_an_zbc2014(x, cf)[1]), LogRange(200.0, 10000.0, 30)))
