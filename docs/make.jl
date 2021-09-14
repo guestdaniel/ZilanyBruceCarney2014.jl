419e618f8a50eb855ad0e4e611d1c31bca25fc56
Daniel Guest <guestusut@gmail.com>
2021-09-13 23:47:07 -0500

    Reorganized gh-pages
    Reorganized gh-pages to better align with what file structure GitHub
    likes

--- docs/make.jl
+++
@@ -1,4 +0,0 @@
-using Documenter
-using AuditoryNerveFiber
-
-makedocs(sitename="My Documentation")
---
+++ make_docs.jl
@@ -0,0 +1,4 @@
+using Documenter
+using AuditoryNerveFiber
+
+makedocs(sitename="AuditoryNerveFiber.jl", source="src_docs", build="docs")
