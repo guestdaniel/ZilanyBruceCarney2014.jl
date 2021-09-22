e331e24de7cbc23e009dfc33cb268791ac663646
Daniel Guest <guestusut@gmail.com>
2021-09-22 11:40:05 -0500

    Added tests for ffGn

--- test/validate_zbc2014.jl
+++ test/validate_zbc2014.jl
@@ -13,8 +13,6 @@ addprocs(3)
 @everywhere using Statistics
 using Plots
 
-# Zilany et al. (2009): Figure 3
-
 """
     zilanyetal2014_figure1()
 
@@ -22,7 +20,7 @@ This function replicates the right-hand-side of Figure 1 from Zilany, Bruce, and
 """
 function zilanyetal2014_figure1(resolution=50)
     # Define function to synthesize stimulus at one particular freq / level
-    @everywhere function stim(freq, level)
+    @everywhere function synthesize_pure_tone(freq, level)
         # Synthesize pure_tone
         stim = ASU.pure_tone(freq, 0.0, 0.050, 100e3)
         # Scale pure pure 
@@ -34,8 +32,10 @@ function zilanyetal2014_figure1(resolution=50)
     end
 
     # Define function that implements model 
-    @everywhere function resp(stim, cf)
-        return mean(ANF.sim_an_zbc2014(ANF.sim_ihc_zbc2014([zeros(500); stim; zeros(500)], cf), cf)[1])
+    @everywhere function simulate_an_response(stim, cf)
+        x = mean(ANF.sim_an_zbc2014(ANF.sim_ihc_zbc2014([zeros(500); stim; zeros(500)], cf), cf)[1])
+        x = x ./ (1 .+ 0.75e-3 * x)  # transform synapse out into firing rate
+        return x
     end
 
     # Define ranges over which we'll iterate
@@ -44,8 +44,56 @@ function zilanyetal2014_figure1(resolution=50)
     domain = [(cf, level) for cf in cfs, level in levels]
 
     # Get responses
-    avg_rates = reshape(pmap(x -> resp(stim(500.0, x[2]), x[1]), hcat(domain...)), (resolution, resolution))
+    avg_rates = reshape(pmap(x -> simulate_an_response(synthesize_pure_tone(500.0, x[2]), x[1]), hcat(domain...)), (resolution, resolution))
 
     # Plot
     heatmap(cfs, levels, transpose(avg_rates), xscale=:log10, color=:jet, clim=(50, 320))
+
+    # Save figure
+    savefig("test/outputs/zilanyetal2014_figure1.png")
+end
+
+
+"""
+    zilanyetal2009_figurex()
+"""
+function zilanyetal2009_figure4(n_rep=20)
+    # Define function to synthesize stimulus
+    @everywhere function synthesize_pure_tone(dur)
+        # Synthesize pure tone of duration dur
+        stim = ASU.pure_tone(10_000.0, 0.0, dur, 100e3)
+        # Set to level
+        stim = ASU.scale_dbspl(stim, 15.0)
+        # Set ramp
+        stim = ASU.cosine_ramp(stim, 0.0025, 100e3)
+        # Zero-pad
+        stim = [zeros(500); stim; zeros(20_000)]
+        # Return
+        return stim
+    end
+    # Define function to run simulation
+    @everywhere function simulate_an_response(dur, n_rep)
+        # Synthesize stimulus 
+        stim = synthesize_pure_tone(dur)
+        # Run simulation
+        x = ANF.sim_synapse_zbc2014(ANF.sim_ihc_zbc2014(stim, 10_000.0; n_rep=n_rep), 10_000.0; frac_noise="actual")
+        x = x ./ (1 .+ 0.75e-3 * x)  # transform synapse into firing rate
+        x = reshape(x, (Int(500 + dur*100e3 + 20e3), n_rep))
+        # Average over resp
+        x = mean(x; dims=2)
+        # Return zero-padded to 1.2 seconds
+        return [x; zeros(Int(1.5*100e3 - length(x)))]
+    end
+    # Run simulation for many levels
+    results = pmap(dur -> simulate_an_response(dur, n_rep), [0.050, 0.10, 0.20, 0.50, 1.0])
+
+    # Visualize
+    t = LinRange(0, 1.5, Int(100e3*1.5))
+    plot(t, results, ylim=(0, 500), label=hcat([0.050, 0.10, 0.20, 0.50, 1.0]...), legendtitle="Duration (s)")
+    hline!([75], color="black", label="")
+    ylabel!("Firing rate (sp/s)")
+    xlabel!("Time (s)")
+
+    # Save figure
+    savefig("test/outputs/zilanyetal2009_figure4.png")
 end
