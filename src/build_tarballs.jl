38f01d38114b3473a0a66f3d75bfe8af9dd9c98c
Daniel Guest <guestusut@gmail.com>
2021-12-16 11:02:18 -0600

    Fixed issue related to fractional Gaussian noise
    I forgot that I had commented out a line in the C source eliminating
    fractional Gaussian noise from the slow-rate adaptation pathway.
    This commit fixes that bug (verificiation incoming).

--- src/build_tarballs.jl
+++ src/build_tarballs.jl
@@ -1,11 +1,12 @@
 using BinaryBuilder
 
 name = "libzbc2014"
-version = v"0.1.0"
+version = v"0.2.1"
 sources = [
     DirectorySource("external")
 ]
 
+# Build for Linux
 script = raw"""
 cd ${WORKSPACE}/srcdir/
 clang -c -Wall -fPIC -Ofast model_IHC.c
@@ -18,7 +19,12 @@ mkdir ${libdir}
 cp libzbc2014.so ${libdir}
 """
 
-platforms = [supported_platforms()[2]]
+platforms = [supported_platforms()[1],     # Linux i686 (glibc)
+             supported_platforms()[2],     # Linux x64 (glibc)
+             supported_platforms()[6],     # Linux i686 (musl)
+             supported_platforms()[7]]     # Linux i686 (musl)
+
+
 
 products = [
     LibraryProduct("libzbc2014", :libzbc2014)
@@ -27,3 +33,26 @@ products = [
 dependencies = []
 
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+
+# Build for Windows 
+# script = raw"""
+# cd ${WORKSPACE}/srcdir/
+# gcc -c model_IHC.c
+# gcc -c model_Synapse.c
+# gcc -c complex.c 
+# gcc -c test.c 
+# gcc -shared -o libzbc2014.dll model_IHC.o model_Synapse.o complex.o test.o
+# echo ${libdir}
+# mkdir ${libdir}
+# cp libzbc2014.dll ${libdir}
+# """
+
+# platforms = [supported_platforms()[13]]    # Windows x64
+
+# products = [
+#     LibraryProduct("libzbc2014", :libzbc2014)
+# ]
+
+# dependencies = []
+
+# build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
